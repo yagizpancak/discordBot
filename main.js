b7b56c986c39908fd327911ca606f0e5d69e4f4f
yagizcan923 <yagizcan923@gmail.com>
2020-06-13 00:49:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -109,7 +109,8 @@ bot.on('message', message=>{
         case 'saat':
 
             var today = new Date();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            var hour = toString(parseInt(today.getHours())+3);
+            var time = (hour) + ":" + today.getMinutes() + ":" + today.getSeconds();
 
             message.channel.send(time);
 
