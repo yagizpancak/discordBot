b5c553cfcd14580d313f78bea586cbca257b6262
yagizcan923 <yagizcan923@gmail.com>
2021-04-17 17:33:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function playSound(channel, voiceConnection, link) {
                 quality:"highestaudio"
             })
 
-            
+
             if (!channel){
                 return;
             }
@@ -65,7 +65,7 @@ function playSound(channel, voiceConnection, link) {
             if(!voiceConnection) 
                 channel.join()
                 .then(function(connection) {
-                const dispatcher = connection.play(stream)
+                const dispatcher = connection.play(link)
                 dispatcher.on("finish", function(){
                     connection.disconnect();
                 });
